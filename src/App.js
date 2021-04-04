0a397e9d4eaab8fd3da2b8d066df58babb430229
Julia Semakhina <contremoi@yandex.ru>
2021-04-04 22:51:38 +0300

    final files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
 const [color, setColor] = useState('');
 const [error, setError] = useState(false);
-const [list, setList] = useState([]);
+const [list, setList] = useState(new Values('#f15025').all(10));
 
 const handleSubmit=(e)=> {
   e.preventDefault();
@@ -18,7 +18,6 @@ setList(colors);
     setError(true)
     console.log(error)
   }
-  
 }
 
   return (
